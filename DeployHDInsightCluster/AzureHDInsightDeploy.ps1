274aa6eac4c1626466e6beba5d6e176eaf346c41
erik <ekringen@outlook.com>
2017-03-01 23:52:23 -0500

    Created initial project and imported files from Azure deployment.

---
+++ DeployHDInsightCluster/AzureHDInsightDeploy.ps1
@@ -0,0 +1,13 @@
+﻿$cred = Get-Credential
+Login-AzureRmAccount -Credential $cred
+
+# To view all subscriptions for your account
+Get-AzureRmSubscription
+
+# To select a default subscription for your current session.
+# This is useful when you have multiple subscriptions.
+Get-AzureRmSubscription -SubscriptionName "Visual Studio Professional with MSDN" | Select-AzureRmSubscription
+
+Switch-AzureRMMode -Name AzureResourceManager
+
+New-AzureRmResourceGroup -Name HDInsight -Location 'South Central US'
